610275d870a1add5ac43afd9f16bf0c2dcde9cec
Haxorico <38023131+Haxorico@users.noreply.github.com>
2022-02-16 19:42:41 +0200

    add rect struct

---
+++ Trainer-Call-of-Duty/lib/data/Rect.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Trainer_Call_of_Duty.lib.data
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/windows/win32/api/windef/ns-windef-rect
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Rect
+    {
+        public int Left, Top, Right, Bottom;
+    }
+}
