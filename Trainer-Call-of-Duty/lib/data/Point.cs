5289ea4882b60c832dea174930065a847594f96c
Haxorico <38023131+Haxorico@users.noreply.github.com>
2022-02-16 19:42:55 +0200

    add point struct

---
+++ Trainer-Call-of-Duty/lib/data/Point.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Trainer_Call_of_Duty.lib.data
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/windows/win32/api/windef/ns-windef-point
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Point
+    {
+        public int X, Y;
+    }
+}
