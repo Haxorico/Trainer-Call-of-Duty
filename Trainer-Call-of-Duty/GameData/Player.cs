740a99a1352d542c7e8247a6ca1698cebe100f99
Haxorico <38023131+Haxorico@users.noreply.github.com>
2022-02-16 20:48:03 +0200

    Init (#1)
    * add gitignore
    
    * add user32.dll class
    
    * add rect struct
    
    * add point struct
    
    * update user32 with more functions
    
    * add overlay form
    
    * add helper U class
    
    * add directx dlls
    
    * add Kernel32 dll import
    
    * add Memory static class helper
    
    * removed some duplicate code and publicize the data
    
    * add offsets and placeholder files for entity and player
    
    * Add some offsets
    
    * filled player with data that might be removed later
    
    * remove unused imports
    
    * removed comments and unused code
    
    * filled entity with data
    
    * close-up

---
+++ Trainer-Call-of-Duty/GameData/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.DirectX;
+using Trainer_Call_of_Duty.Helpers;
+namespace Trainer_Call_of_Duty.GameData
+{
+    public static class Player
+    {
+        public static Matrix ViewMatrixOpenGL { get; set; }
+        public static Matrix ViewMatrixDirectX { get; set; }
+
+        public static void Update()
+        {
+            
+            IntPtr tmp = Memory.AddOffsetToIntPtr(Offsets.MOD_BASE_ADR, Offsets.pViewMatrix);
+            ViewMatrixOpenGL = Memory.GetViewMatrix_OpenGL(tmp);
+            ViewMatrixDirectX = Memory.GetViewMatrix_DirectX(tmp);
+        }
+    }
+}
---
+++ Trainer-Call-of-Duty/Overlay.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Trainer_Call_of_Duty.lib;
+using Trainer_Call_of_Duty.lib.data;
+
+namespace Trainer_Call_of_Duty
+{
+    public partial class Overlay : Form
+    {
+        const string WINDOW_NAME = "Sniper Ghost Warrior 3";
+        IntPtr handle = User32.FindWindow(null, WINDOW_NAME);
+        bool windowActive { get; set; }
+        bool isShowing { get; set; }
+        List<Rectangle> emptyRects = new List<Rectangle>();
+        List<Rectangle> fullRects = new List<Rectangle>();
+        List<string> emptyRectText = new List<string>();
+        List<Pen> emptyRectStyles = new List<Pen>();
+        List<Color> fullRectStyles = new List<Color>();
+
+
+        public void UpdateOverlayLocation()
+        {
+            //make the overlay the same size as the game window
+            Rect gameRect;
+            User32.GetWindowRect(handle, out gameRect);
+            this.Size = new Size(gameRect.Right - gameRect.Left, gameRect.Bottom - gameRect.Top);
+
+            //move the overlay to the game window
+            this.Top = gameRect.Top;
+            this.Left = gameRect.Left;
+
+            windowActive = handle == User32.GetForegroundWindow();
+
+            //refresh the overlay?
+            if (windowActive)
+            {
+                if (isShowing)
+                    this.Refresh();
+                else
+                {
+                    isShowing = true;
+                    this.Show();
+                }
+            }
+            else
+            {
+                isShowing = false;
+                this.Hide();
+            }
+
+        }
+
+        public int AddRect(int x, int y, int width, int height, Color c, float thickness = 3, bool fill = false)
+        {
+            Rectangle r = new Rectangle(x, y, width, height);
+            if (fill)
+            {
+                fullRects.Add(r);
+                fullRectStyles.Add(c);
+                return fullRects.Count;
+            }
+            else
+            {
+                emptyRectText.Add("");
+                emptyRects.Add(r);
+                emptyRectStyles.Add(new Pen(c, thickness));
+                return emptyRects.Count;
+            }
+
+        }
+
+        public void UpdateRect(int index, int x, int y, int width, int height, Color c, float thickness = -1, string msg = "")
+        {
+            if (thickness < 0)
+                thickness = emptyRectStyles[index].Width;
+            emptyRectText[index] = msg;
+            emptyRects[index] = new Rectangle(x, y, width, height);
+            emptyRectStyles[index].Color = c;
+            emptyRectStyles[index].Width = thickness;
+        }
+
+        public void UpdateFullRect(int index, int x, int y, int width, int height, Color c)
+        {
+            fullRects[index] = new Rectangle(x, y, width, height);
+            fullRectStyles[index] = c;
+        }
+
+        public Overlay()
+        {
+            InitializeComponent();
+        }
+
+        private void Overlay_Load(object sender, EventArgs e)
+        {
+             //make the overlay transparent
+            this.BackColor = Color.Wheat;
+            this.TransparencyKey = Color.Wheat;
+
+            //make the overlay always on top
+            this.TopMost = true;
+
+            //remove the borders
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            //make the overlay click-through
+            uint initialStyle = User32.GetWindowLong(this.Handle, -20);
+            User32.SetWindowLong(this.Handle, -20, initialStyle | 0x80000 | 0x20);
+
+            windowActive = false;
+            isShowing = false;
+        }
+
+        private void Overlay_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            for (int i = 0; i < emptyRects.Count; i++)
+            {
+                g.DrawRectangle(emptyRectStyles[i], emptyRects[i]);
+                g.DrawString(emptyRectText[i], this.Font, new SolidBrush(Color.Yellow), (float)emptyRects[i].X, (float)emptyRects[i].Y);
+            }
+            for (int i = 0; i < fullRects.Count; i++)
+            {
+                SolidBrush sb = new SolidBrush(fullRectStyles[i]);
+                g.FillRectangle(sb, fullRects[i]);
+            }
+        }
+    }
+}
