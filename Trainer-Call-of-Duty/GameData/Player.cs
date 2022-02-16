992c248c206a538d550620e99e26ba8c5179b80a
Haxorico <38023131+Haxorico@users.noreply.github.com>
2022-02-16 20:45:11 +0200

    removed comments and unused code

--- Trainer-Call-of-Duty/GameData/Player.cs
+++ Trainer-Call-of-Duty/GameData/Player.cs
@@ -10,15 +10,10 @@ namespace Trainer_Call_of_Duty.GameData
 
         public static void Update()
         {
-            //IntPtr tmp = Memory.AddOffsetToIntPtr(Address, Offsets.ePosition03);
-            //Origin = Memory.GetVector3(tmp);
+            
             IntPtr tmp = Memory.AddOffsetToIntPtr(Offsets.MOD_BASE_ADR, Offsets.pViewMatrix);
             ViewMatrixOpenGL = Memory.GetViewMatrix_OpenGL(tmp);
             ViewMatrixDirectX = Memory.GetViewMatrix_DirectX(tmp);
-            //CamHeight = Memory.GetIntFromAddress(Memory.AddOffsetToIntPtr(Address, Offsets.pCamHeightOffset));
-            //CamPosition = new Vector3(Origin.X, Origin.Y, (Origin.Z + Convert.ToSingle(CamHeight)));
-            //Position = Memory.GetVector3(Address1);
-            // Position = Memory.GetVector3(Address2 + Offsets.ePosition03);
         }
     }
 }
