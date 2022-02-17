e855d959a28ad7679ab0ad1bfb20ae9a02d5166a
Haxorico <38023131+Haxorico@users.noreply.github.com>
2022-02-17 20:14:22 +0200

    update player class to read more memory (hp and the like)

--- Trainer-Call-of-Duty/GameData/Player.cs
+++ Trainer-Call-of-Duty/GameData/Player.cs
@@ -5,8 +5,12 @@ namespace Trainer_Call_of_Duty.GameData
 {
     public static class Player
     {
-        public static Matrix ViewMatrixOpenGL { get; set; }
-        public static Matrix ViewMatrixDirectX { get; set; }
+        public static Matrix ViewMatrixOpenGL { get; private set; }
+        public static Matrix ViewMatrixDirectX { get; private set; }
+        public static IntPtr Address { get; private set; }
+        public static IntPtr Part2 {  get; private set; }
+        public static IntPtr NextEntity { get; private set; }   
+        public static int CurrentHP { get; private set; }
 
         public static void Update()
         {
@@ -14,6 +18,13 @@ namespace Trainer_Call_of_Duty.GameData
             IntPtr tmp = Memory.AddOffsetToIntPtr(Offsets.ADR_BASE, Offsets.pViewMatrix);
             ViewMatrixOpenGL = Memory.GetViewMatrix_OpenGL(tmp);
             ViewMatrixDirectX = Memory.GetViewMatrix_DirectX(tmp);
+
+            Address = Memory.AddOffsetToIntPtr(Offsets.ADR_MOD_ENGINE, Offsets.eFirstEntityAdr);
+            CurrentHP = Memory.GetIntFromAddress(new IntPtr((uint)Address + Offsets.eCurrentHP));
+            Part2 = new IntPtr(Memory.GetIntFromAddress(Memory.AddOffsetToIntPtr(Address, Offsets.ePart2)));
+            NextEntity = new IntPtr(Memory.GetIntFromAddress(Memory.AddOffsetToIntPtr(Part2, Offsets.eNextEntity)));
+
+
         }
     }
 }
